b237068d4104ae6721bf280db46bdebf28536d2e
Erick Lopez <lrerick20001430@gmail.com>
2024-06-27 18:03:19 -0600

    primer commit

---
+++ TareasAPI_903/Controllers/TareasController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.AccessControl;
+using TareasAPI_903.Models;
+
+namespace TareasAPI_903.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TareasController : ControllerBase
+    {
+        //Variable de ccontexto de BD
+        private readonly Bdtareas903Context _baseDatos;
+
+        public TareasController(Bdtareas903Context baseDatos)
+        {
+            this._baseDatos = baseDatos;
+        }
+
+        //Método GET ListarTareas
+        [HttpGet]
+        [Route("ListaTareas")]
+        public async Task<IActionResult> Lista()
+        {
+            var listaTareas = await 
+                _baseDatos.Tareas.ToListAsync();
+            return Ok(listaTareas);
+        }
+
+        //Método POST AgregarTarea
+        [HttpPost]
+        [Route("AgregarTarea")]
+        public async Task<IActionResult> Agregar([FromBody] Tarea request)
+        {
+            await _baseDatos.Tareas.AddAsync(request);
+            await _baseDatos.SaveChangesAsync();
+            return Ok(request);
+        }
+
+        //Método DELETE EliminarTarea
+        [HttpDelete]
+        [Route("EliminarTarea/(id: int)")]
+        public async Task<IActionResult> Eliminar(int id)
+        {
+            var tareaEliminar = await
+                _baseDatos.Tareas.FindAsync(id);
+
+            if (tareaEliminar == null)
+            {
+                return BadRequest("La tarea no existe");
+            }
+
+            _baseDatos.Tareas.Remove(tareaEliminar);
+            await _baseDatos.SaveChangesAsync();
+            return Ok();
+        }
+        //Cross-Origin Resource Sharing
+
+    }
+}
---
+++ TareasAPI_903/Models/Bdtareas903Context.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace TareasAPI_903.Models;
+
+public partial class Bdtareas903Context : DbContext
+{
+    public Bdtareas903Context()
+    {
+    }
+
+    public Bdtareas903Context(DbContextOptions<Bdtareas903Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Tarea> Tareas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){ }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Tarea>(entity =>
+        {
+            entity.HasKey(e => e.IdTarea).HasName("PK__Tarea__756A5402B86D9CD4");
+
+            entity.ToTable("Tarea");
+
+            entity.Property(e => e.IdTarea).HasColumnName("idTarea");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("nombre");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
